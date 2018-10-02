8e9800652ca0c45eb181db2ea06904d99ec2c197
vikasgautam <vikas.gautam18@gmail.com>
2018-10-02 21:42:57 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "SparkKafkaWithCheckpointing"
+
+version := "0.1"
+
+scalaVersion := "2.11.0"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "3.0.4" % "test"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0"
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % "2.2.0"
---
+++ src/main/scala/com/wordpress/technicado/MainWithoutCheckpointing.scala
@@ -0,0 +1,42 @@
+package com.wordpress.technicado
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+object MainWithoutCheckpointing {
+
+  def main(args: Array[String]): Unit = {
+    val conf: SparkConf = new SparkConf().setAppName("MainWithoutCheckpointing").setMaster("local[*]")
+    val ssc: StreamingContext = new StreamingContext(conf, Seconds(10))
+
+    val kafkaParams: Map[String, Object] = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9092",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "some_group",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val topics: Array[String] = Array("test_source_topic")
+    val stream: InputDStream[ConsumerRecord[String, String]] = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+
+    val inputStream: DStream[(String, String)] = stream.map(record => (record.key, record.value))
+
+    inputStream.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
